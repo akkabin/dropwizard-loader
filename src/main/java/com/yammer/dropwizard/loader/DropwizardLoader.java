a9f7ca4c3b4611a99092938f2e813996e5ef638f
brianchen <briangxchen@gmail.com>
2013-05-15 15:20:51 +1000

    service loader

--- src/main/java/com/yammer/dropwizard/loader/DropwizardLoader.java
+++ src/main/java/com/yammer/dropwizard/loader/DropwizardLoader.java
@@ -1,25 +1,12 @@
 package com.yammer.dropwizard.loader;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import javax.ws.rs.ext.ExceptionMapper;
-
-import com.sun.jersey.api.core.ResourceConfig;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
-import com.yammer.dropwizard.loader.exception.GenericExceptionMapper;
-import com.yammer.dropwizard.loader.exception.RuntimeExceptionMapper;
-import com.yammer.dropwizard.loader.health.TemplateHealthCheck;
 
 public class DropwizardLoader extends Service<AppConfiguration> {
-	private static final String APP_NAME="Dropwizard-Loader";
-	
-	public static void main(String[] args) throws Exception {
-		new DropwizardLoader().run(args);
-	}
+	private static final String APP_NAME = "Dropwizard-Loader";
+	private static final ServiceLoader loader = new ServiceLoader();
 
 	@Override
 	public void initialize(Bootstrap<AppConfiguration> bootstrap) {
@@ -28,31 +15,14 @@ public class DropwizardLoader extends Service<AppConfiguration> {
 
 	@Override
 	public void run(AppConfiguration configuration, Environment environment) {
-		final String template = configuration.getTemplate();
-		new ServiceLoader().init(configuration, environment);
-
-		environment.addHealthCheck(new TemplateHealthCheck(template));
-
-		// Remove all of Dropwizard's custom ExceptionMappers
-		ResourceConfig jrConfig = environment.getJerseyResourceConfig();
-		Set<Object> dwSingletons = jrConfig.getSingletons();
-		List<Object> singletonsToRemove = new ArrayList<Object>();
-
-		for (Object s : dwSingletons) {
-			if (s instanceof ExceptionMapper
-					&& s.getClass().getName()
-							.startsWith("com.yammer.dropwizard.jersey.")) {
-				singletonsToRemove.add(s);
-			}
-		}
-
-		for (Object s : singletonsToRemove) {
-			jrConfig.getSingletons().remove(s);
-		}
-
-		// Register the custom ExceptionMapper(s)
-		environment.addProvider(new RuntimeExceptionMapper());
-		environment.addProvider(new GenericExceptionMapper());
+		loader.loadResources(configuration, environment);
+		loader.loadExceptionMapper(configuration, environment);
+		loader.loadHealthTemplate(configuration, environment);
 	}
+	
+	public static void main(String[] args) throws Exception {
+		new DropwizardLoader().run(args);
+	}
+
 
 }
--- src/main/java/com/yammer/dropwizard/loader/ServiceLoader.java
+++ src/main/java/com/yammer/dropwizard/loader/ServiceLoader.java
@@ -1,22 +1,62 @@
 package com.yammer.dropwizard.loader;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.ext.ExceptionMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.jersey.api.core.ResourceConfig;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.loader.exception.GenericExceptionMapper;
+import com.yammer.dropwizard.loader.exception.RuntimeExceptionMapper;
+import com.yammer.dropwizard.loader.health.TemplateHealthCheck;
 
 public class ServiceLoader {
 
 	private static final Logger LOG = LoggerFactory
 			.getLogger(ServiceLoader.class);
 
-	public void init(AppConfiguration configuration, Environment environment) {
+	public void loadResources(AppConfiguration configuration,
+			Environment environment) {
 		List<String> resources = configuration.getResources();
 		for (String clazz : resources) {
 			environment.addResource(makeClassInstance(clazz));
 		}
+
+	}
+
+	public void loadHealthTemplate(AppConfiguration configuration,
+			Environment environment) {
+		environment.addHealthCheck(new TemplateHealthCheck(configuration
+				.getTemplate()));
+
+	}
+
+	public void loadExceptionMapper(AppConfiguration configuration,
+			Environment environment) {
+		ResourceConfig jrConfig = environment.getJerseyResourceConfig();
+		Set<Object> dwSingletons = jrConfig.getSingletons();
+		List<Object> singletonsToRemove = new ArrayList<Object>();
+
+		for (Object s : dwSingletons) {
+			if (s instanceof ExceptionMapper
+					&& s.getClass().getName()
+							.startsWith("com.yammer.dropwizard.jersey.")) {
+				singletonsToRemove.add(s);
+			}
+		}
+
+		for (Object s : singletonsToRemove) {
+			jrConfig.getSingletons().remove(s);
+		}
+
+		environment.addProvider(new RuntimeExceptionMapper());
+		environment.addProvider(new GenericExceptionMapper());
+
 	}
 
 	@SuppressWarnings("unchecked")
